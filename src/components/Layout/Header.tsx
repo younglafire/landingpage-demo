600087d7d880d2d06b38a02776836c1f2917fd3b
Đoàn Gia Bảo <younglafire@gmail.com>
2025-07-06 21:47:34 +0700

    Phóng to logo và xóa chữ Study VHU ở header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -26,10 +26,7 @@ export const Header: React.FC<HeaderProps> = ({ onMenuToggle, isMobileMenuOpen }
           <div className="flex justify-between items-center h-16">
             {/* Logo */}
             <div className="flex items-center space-x-2">
-              <img src="/image-Photoroom.png" alt="Study VHU Logo" className="w-8 h-8" />
-              <span className="text-xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
-                Study VHU
-              </span>
+              <img src="/image-Photoroom.png" alt="Study VHU Logo" className="w-12 h-12" />
             </div>
             
             {/* Desktop Navigation */}
